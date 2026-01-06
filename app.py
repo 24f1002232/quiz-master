1b26b8cac8e2e18aca0210150cc1c17c748c389f
24f1002232 <24f1002232@ds.study.iitm.ac.in>
2026-01-06 13:10:46 +0530

    Use password hashing (werkzeug) and read SECRET_KEY from env; update dev branch

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask,render_template
 from controllers.database import db
 from controllers.models import *
+from werkzeug.security import generate_password_hash
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-app.config['SECRET_KEY'] = 'thisissecretkey'
+# Read SECRET_KEY from environment with a safe development fallback
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'thisissecretkey')
 db.init_app(app)
 
 with app.app_context():
@@ -15,14 +18,12 @@ with app.app_context():
         admin = User(
             name = "admin",
             email = "admin@gmail.com",
-            password = "admin123",
+            password = generate_password_hash("admin123"),
             role = "admin"
         )
         db.session.add(admin)
         db.session.commit()
-    
 
-    
 
 from controllers.routes import *
 
